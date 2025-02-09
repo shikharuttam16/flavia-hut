d461807946d0c52f6270ee26636e65a1c940fd22
Shikhar Uttma <uttam16shikhar@gmail.com>
2025-02-09 23:40:49 +0530

    change made for address

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -120,7 +120,7 @@ const Header = ({ onFAQClick }) => {
           <div>
             <span className="mr-1 hidden sm:inline">📍 Store Location:</span>
             <span className="mr-1 hidden sm:inline">
-              #985/986 Housing Board Colony Sector 10 Ambala City
+              #985/986 Housing Board Colony Sector 10 Ambala 
             </span>
           </div>
           <div className="flex gap-2 items-center">
