2086b7ac4d1a0923a92c3b3ae85bd097c90a3f64
Amit1pal <amitpal00010@gmail.com>
2025-02-10 20:36:56 +0530

    add to cart button design changed

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -76,10 +76,7 @@ const ProductCard = ({ product, handleAddToCart, wishlistHandler }) => {
             </div>
 
             {/* Add to Cart */}
-            <FaShoppingCart
-              onClick={(e) => handleAddToCart(e, product?._id)}
-              className="cursor-pointer" // Add cursor pointer for better UX
-            />
+            <button onClick={(e) => handleAddToCart(e, product?._id)} className="cursor-pointer bg-[#28AD00] text-white px-4 py-2 rounded-[6px] font-barlow font-semibold text-[16px] leading-[26px] tracking-[0%] text-center"> Add to Cart</button>
           </div>
 
           <div>
