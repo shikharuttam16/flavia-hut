cb36f84143f4f1a0ff1b538af71229e640b49300
Shikhar Uttma <uttam16shikhar@gmail.com>
2025-02-16 15:30:50 +0530

    shopping cart is added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -267,19 +267,19 @@ const Header = ({ onFAQClick }) => {
                     onClick={toggleProductsDropdown}
                   >
                     Products
-                    <svg
+                    {/* <svg
                       className="h-5 w-5 flex-none text-gray-400"
                       viewBox="0 0 20 20"
                       fill="currentColor"
                       aria-hidden="true"
                       data-slot="icon"
-                    >
-                      <path
+                    > */}
+                      {/* <path
                         fill-rule="evenodd"
                         d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
                         clip-rule="evenodd"
-                      />
-                    </svg>
+                      /> */}
+                    {/* </svg> */}
                   </button>
                   {isProductsDropdownOpen && (
                     <div className="absolute mt-2 w-48 bg-white border shadow-lg rounded-lg text-black">
