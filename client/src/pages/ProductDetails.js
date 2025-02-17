612fac3a9ca78d52d32fac1f9ab6c0f127aa4a94
Amit1pal <amitpal00010@gmail.com>
2025-02-17 21:33:46 +0530

    product detail page css changed

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -251,20 +251,22 @@ const ProductDetails = () => {
               <p className="font-titillium font-semibold text-[14px] leading-[26.18px] tracking-[0%] text-[#424750] mb-3">
                 Offer Price: <span className="font-barlow font-semibold text-[22px] leading-[26px] tracking-[0%] text-[#56CE00]">{displayINRCurrency(data.sellingPrice)}</span> <span className="font-titillium font-normal text-[14px] leading-[14px] tracking-[0%] text-[#424750]">(incl. of all taxes)</span>
               </p>
-              <div className="flex items-center space-x-2 border border-gray-400 rounded-md p-2 w-max">
-                <button
-                  onClick={decreaseQuantity}
-                  className="px-3 py-1 bg-gray-200 hover:bg-gray-300 text-gray-700 font-bold rounded"
-                >
-                  −
-                </button>
-                <span className="text-lg font-medium w-8 text-center">{quantity}</span>
-                <button
-                  onClick={increaseQuantity}
-                  className="px-3 py-1 bg-gray-200 hover:bg-gray-300 text-gray-700 font-bold rounded"
-                >
-                  +
-                </button>
+              <div className="font-titillium font-semibold text-[14px] leading-[26.18px] tracking-[0%] text-[#424750] mb-3">
+              Quantity: <div className="flex items-center space-x-2 border border-gray-400 rounded-md p-2 w-max">
+                  <button
+                    onClick={decreaseQuantity}
+                    className="px-3 py-1 bg-gray-200 hover:bg-gray-300 text-gray-700 font-bold rounded"
+                  >
+                    −
+                  </button>
+                  <span className="text-lg font-medium w-8 text-center">{quantity}</span>
+                  <button
+                    onClick={increaseQuantity}
+                    className="px-3 py-1 bg-gray-200 hover:bg-gray-300 text-gray-700 font-bold rounded"
+                  >
+                    +
+                  </button>
+                </div>
               </div>
               <div className="flex items-center gap-3 my-2">
                 <button className="w-[292.5px] h-[48px] rounded-[4px] px-3 py-2 font-barlow font-bold text-[16px] leading-[26px] tracking-[0%] text-center text-white bg-[#FF8C00] hover:bg-[#FF8C00]" onClick={(e) => handleAddToCart(e, data?._id)}> Add To Cart </button>
