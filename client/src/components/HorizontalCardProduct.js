b2a3c479855a9eb0547a1ea95d9bac80b5885a96
Amit1pal <amitpal00010@gmail.com>
2025-02-18 23:48:19 +0530

    slider for the home page css changed

--- client/src/components/HorizontalCardProduct.js
+++ client/src/components/HorizontalCardProduct.js
@@ -71,11 +71,9 @@ const HorizontalCardProduct = ({ category, heading1, heading2 }) => {
           spaceBetween={10} // Reduced space between items
           slidesPerView={1.5}
           breakpoints={{
-            576: { slidesPerView: 1, spaceBetween: 10 },
             640: { slidesPerView: 1, spaceBetween: 10 },
             768: { slidesPerView: 2, spaceBetween: 10 },
-            992: { slidesPerView: 3, spaceBetween: 10 },
-            1024: { slidesPerView: 4, spaceBetween: 10 },
+            1024: { slidesPerView: 3, spaceBetween: 10 },
             1200: { slidesPerView: 5, spaceBetween: 10 },
           }}
           navigation
