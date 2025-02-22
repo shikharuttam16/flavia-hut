2a9d7f261901a99165990e14e40be4070df5bead
Shikhar Uttma <uttam16shikhar@gmail.com>
2025-02-22 12:15:23 +0530

    All categories

--- client/src/pages/AllCategories.js
+++ client/src/pages/AllCategories.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function AllCategories() {
   return (
-    <div>AllCategories</div>
-  )
+    <div className="bg-white pb-4 border overflow-y-scroll">
+      <table className="w-full userTable">
+        <thead>
+          <tr className="bg-black text-white">
+            <th>Sr.</th>
+            <th>Name</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+  );
 }
 
-export default AllCategories
+export default AllCategories;
