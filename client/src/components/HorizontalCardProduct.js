1d72cba7e9525ddbc4706ec8332b5c801ecf25ad
Amit1pal <amitpal00010@gmail.com>
2025-02-23 13:13:02 +0530

    slider naviagtion disabled

--- client/src/components/HorizontalCardProduct.js
+++ client/src/components/HorizontalCardProduct.js
@@ -69,7 +69,7 @@ const HorizontalCardProduct = ({ category, heading1, heading2 }) => {
       <Swiper
         modules={[Navigation]}
         spaceBetween={10}
-        slidesPerView={5} // Ensures proper alignment
+        slidesPerView={1} // Ensures proper alignment
         breakpoints={{
           640: { slidesPerView: 1, spaceBetween: 10 },
           768: { slidesPerView: 2, spaceBetween: 10 },
